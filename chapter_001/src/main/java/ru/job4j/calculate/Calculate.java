9ecf27580d89344f4ce44c6908e76a657e2af010
Andrey Mashkov <mashkov@farmnet.ru>
2018-06-20 22:55:41 +0400

    1.3. JavDoc. Code convetion[#58009]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate
+ *
+ * @author Mashkov Andrey
+ * @version 1.0
+ * @since 20.06.2018
+ */
 public class Calculate {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World");		
-	}
-	
+
+    /**
+     * Конструктор, выводит строки в консоль
+     * @param args массив строковых аргументов
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Mashkov (mailto:andmmm@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
